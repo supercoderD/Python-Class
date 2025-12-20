05f8f4449ec97122ae9231e98f7a74f618d159c1
supercoderD <darshugandhi1@gmail.com>
2025-12-19 18:20:48 -0800

    initital commit

--- first.py
+++ first.py
@@ -1,1 +1,9 @@
 print("Hello") 
+age=6
+name=age+5
+print(age+name)
+#Divide two numbers 
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number"))
+div=x/y
+print("The answer is", div)
