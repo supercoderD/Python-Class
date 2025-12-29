4d15d9f58fb865b357a7612d6533bc74e0ec39e2
supercoderD <darshugandhi1@gmail.com>
2025-12-28 18:17:12 -0800

    initial commit

---
+++ datatype.py
@@ -0,0 +1,18 @@
+# name="Darsh" #str
+# age=10 #integer
+# marks=97.67#float
+# ispassed=True#boolean
+# M=str(marks)
+# print(M,type(M))
+#string ops
+s="i am cool" 
+#length
+print(len(s))
+#indexing
+print(s[6])
+#slicing
+print(s[5:9])
+c="codingal" 
+#reverse
+print(c[::-1])
+    
