a1f643a8f617cbcbbb3146d33a57b9b1c8ea012d
supercoderD <darshugandhi1@gmail.com>
2026-01-30 18:16:27 -0800

    initial commit

---
+++ binary.py
@@ -0,0 +1,30 @@
+# #Python program to illustrate the use if 'is' identity operator
+# x=5
+# if(type(x) is int):
+#     print("true")
+# else:
+#     print("false")
+
+
+# y=5.5
+# if (type(y) is not float):
+#     print("true")
+# else:
+#     print("false")
+
+# a=20
+# b=20
+# if(a is b):
+#     print(" x & y have the SAME identity")
+# else:
+#     print("x & y do not have the SAME identity")
+
+# c=30
+# if (c is not a):
+#     print("x & y have DIFFERENT identity ")
+
+d= 10
+e= -10
+#print bitwise right shift operator
+print("d >> 1 =", d >>1)
+print("e << 1 =", e <<1)
