4fee2e35bc536f6e1391c826d014dbdc886dc97b
supercoderD <darshugandhi1@gmail.com>
2026-08-07 19:30:06 -0700

    initial commit

---
+++ test4.py
@@ -0,0 +1,27 @@
+class Book:
+    isborrowed=False
+    def __init__(self, title, author):
+        self.title=title
+        self.author=author
+
+    def borrow(self):
+        if isborrowed==False:
+            isborrowed=True
+            print("This is a confomation message that you have borrowed a book.")
+        else:
+            print("Sorry, this book has already been borrowed by another user.")
+   
+    def returnbook(self):
+        if isborrowed==True:
+            isborrowed=False
+            print("This is a conformation that you have successfully returned your book.")
+
+book1=Book("Harry Potter and the Sorceror's Stone", "J.K Rowling")
+book1.borrow() 
+book1.returnbook()
+book2=Book("The Alien Attack", "Darsh Gandhi")
+book2.borrow() 
+book2.returnbook()
+book3=Book("Middle School: Get me out of here!", "James Patterson")
+book3.borrow() 
+book3.returnbook()
